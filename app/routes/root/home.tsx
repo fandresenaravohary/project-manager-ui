520fe3765b509d39b1774e9c8a2dd54fddf435e7
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-06-17 20:42:43 +0300

    feat: auth structure

--- app/routes/root/home.tsx
+++ app/routes/root/home.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import type { Route } from '../../+types/root';
-import { Button } from '@/components/ui/button';
+import React from "react";
+import type { Route } from "../../+types/root";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "TaskHub" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Welcome to TaskHub!" },
   ];
 }
 
 const HomePage = () => {
   return (
-    <div>
-        <Button>Click me</Button>
+    <div className="w-full h-screen flex items-center justify-center gap-4">
+      <Link to={"sign-in"}>
+        <Button className="bg-blue-500 text-white ">Login</Button>
+      </Link>
+      <Link to={"sign-up"}>
+        <Button className="bg-blue-500 text-white ">Sign Up</Button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
