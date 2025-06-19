ecb1e5eb67468ea1c5b5ebea63887f5af6fcbaa2
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-06-19 10:15:14 +0300

    feat: sign-in page

--- app/routes/auth/Sign-in.tsx
+++ app/routes/auth/Sign-in.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { signInSchema } from "@/lib/schema";
+import React from "react";
+import { useForm } from "react-hook-form";
+import type { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+type SigninFormData = z.infer<typeof signInSchema>;
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const form = useForm<SigninFormData>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-export default SignIn
+  const handleOnSubmit = (values: SigninFormData) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-muted/40 p-4">
+      <Card className="max-w-md w-full shadow-xl">
+        <CardHeader>
+          <CardTitle>Welcome Back</CardTitle>
+          <CardDescription>Sign in to your account to continue</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleOnSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email Address</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="email@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignIn;
