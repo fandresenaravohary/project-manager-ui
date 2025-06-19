8ab0689384d5e48193917caa030c02960a37165b
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-06-19 10:45:09 +0300

    feat: sign-in page

--- app/routes/auth/Sign-in.tsx
+++ app/routes/auth/Sign-in.tsx
@@ -7,6 +7,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -19,6 +20,8 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
 type SigninFormData = z.infer<typeof signInSchema>;
 
@@ -38,13 +41,18 @@ const SignIn = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-muted/40 p-4">
       <Card className="max-w-md w-full shadow-xl">
-        <CardHeader>
-          <CardTitle>Welcome Back</CardTitle>
-          <CardDescription>Sign in to your account to continue</CardDescription>
+        <CardHeader className="text-center mb-5">
+          <CardTitle className="text-2xl font-bold">Welcome Back</CardTitle>
+          <CardDescription className="text-sm text-muted-foreground">
+            Sign in to your account to continue
+          </CardDescription>
         </CardHeader>
         <CardContent>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleOnSubmit)}>
+            <form
+              onSubmit={form.handleSubmit(handleOnSubmit)}
+              className="space-y-6"
+            >
               <FormField
                 control={form.control}
                 name="email"
@@ -62,8 +70,43 @@ const SignIn = () => {
                   </FormItem>
                 )}
               />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="••••••••"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" className="w-full">
+                Sign in
+              </Button>
             </form>
           </Form>
+
+          <CardFooter>
+            <div>
+              <p className="text-sm text-muted-foreground">
+                Don&apos;t have an account?{" "}
+                <Link
+                  to="/sign-up"
+                  className="text-primary hover:underline"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </div>
+          </CardFooter>
         </CardContent>
       </Card>
     </div>
