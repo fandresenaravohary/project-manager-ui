61a011db2a8895c55a96a567e615ebac08818f0e
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-07-05 21:02:23 +0300

    chore: add react-query-provider & auth-context

---
+++ app/provider/auth-context.tsx
@@ -0,0 +1,38 @@
+import type { User } from "@/types";
+import { createContext, useContext, useState } from "react";
+
+interface AuthContextType {
+  user: User | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const login = async (email: string, password: string) => {
+    console.log(email, password);
+  };
+
+  const logout = async () => {
+    console.log("Log out");
+  };
+
+  const values = { user, isAuthenticated, isLoading, login, logout };
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
